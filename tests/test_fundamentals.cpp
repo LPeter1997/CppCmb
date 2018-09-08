466fb57c57cd1d4e7b803ddf95fc420234d73687
LPeter1997 <lenkefi.peti@gmail.com>
2018-09-08 20:15:51 +0200

    Added tests for custom matching, opt and seq combinators

--- tests/test_fundamentals.cpp
+++ tests/test_fundamentals.cpp
@@ -15,4 +15,72 @@ TEST_CASE("'one' takes a single element from the current position", "[one]") {
 	REQUIRE(result);
 	REQUIRE(std::get<0>(result->first) == 1);
 	REQUIRE(std::distance(std::cbegin(v), result->second) == 1);
+
+	auto result2 = pv::one(std::cbegin(v) + 1);
+
+	REQUIRE(result2);
+	REQUIRE(std::get<0>(result2->first) == 2);
+	REQUIRE(std::distance(std::cbegin(v), result2->second) == 2);
+}
+
+template <int Num>
+static constexpr bool match_pred(int n) {
+	return n == Num;
+}
+
+template <int Num>
+static constexpr auto match = pv::one[pv::filter(pv::fn<match_pred<Num>>)];
+
+TEST_CASE("A constructed match functionality accepts only the predicate", "[match]") {
+	int_vector v = { 1, 2, 3 };
+
+	auto result = match<1>(std::cbegin(v));
+	REQUIRE(result);
+	REQUIRE(std::get<0>(result->first) == 1);
+	REQUIRE(std::distance(std::cbegin(v), result->second) == 1);
+
+	auto result2 = match<2>(std::cbegin(v));
+	REQUIRE(!result2);
+}
+
+TEST_CASE("'opt' always succeeds, but does not always consume", "[opt]") {
+	int_vector v = { 1, 2, 3 };
+
+	SECTION("on matching the parser advances") {
+		auto result = pv::opt(match<1>)(std::cbegin(v));
+
+		REQUIRE(result);
+		REQUIRE(std::get<0>(result->first));
+		REQUIRE(std::distance(std::cbegin(v), result->second) == 1);
+	}
+	SECTION("when not matching, still succeed, but no advancement") {
+		auto result = pv::opt(match<2>)(std::cbegin(v));
+
+		REQUIRE(result);
+		REQUIRE(!std::get<0>(result->first));
+		REQUIRE(std::distance(std::cbegin(v), result->second) == 0);
+	}
+}
+
+TEST_CASE("'seq' succeeds when all it's elements succeed", "[seq]") {
+	int_vector v = { 1, 2, 3, 4, 5 };
+
+	SECTION("it succeeds when it matches all the way") {
+		auto result = (match<1> & match<2> & match<3> & match<4>)(std::cbegin(v));
+		REQUIRE(result);
+		REQUIRE(result->first == std::make_tuple(1, 2, 3, 4));
+		REQUIRE(std::distance(std::cbegin(v), result->second) == 4);
+	}
+	SECTION("it can fail right at the beginning") {
+		auto result = (match<2> & match<2> & match<3> & match<4>)(std::cbegin(v));
+		REQUIRE(!result);
+	}
+	SECTION("it can fail in the middle") {
+		auto result = (match<1> & match<2> & match<4> & match<4>)(std::cbegin(v));
+		REQUIRE(!result);
+	}
+	SECTION("it can fail at the end") {
+		auto result = (match<1> & match<2> & match<3> & match<5>)(std::cbegin(v));
+		REQUIRE(!result);
+	}
 }
