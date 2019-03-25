b2d9ee764d5079a54345eb711ed8390c6d64bb5f
LPeter1997 <lenkefi.peti@gmail.com>
2019-03-25 11:55:56 +0100

    Added the result type, combinator base.

--- cppcmb.hpp
+++ cppcmb.hpp
@@ -122,7 +122,7 @@ namespace cppcmb {
             is_detected_convertible<To, Op, Args...>::value;
 
         /**
-         * Iterator concept checking.
+         * Reader concept checking.
          */
         template <typename T>
         using element_at_t =
@@ -137,6 +137,15 @@ namespace cppcmb {
             is_detected<element_at_t, T>,
             is_detected<msize_t, T>
         >;
+
+        /**
+         * @see https://en.cppreference.com/w/cpp/types/remove_cvref
+         */
+        template <typename T>
+        struct remove_cvref : std::remove_cv<std::remove_reference_t<T>> {};
+
+        template <typename T>
+        using remove_cvref_t = typename remove_cvref<T>::type;
 
     } /* namespace detail */
 
@@ -192,6 +201,212 @@ namespace cppcmb {
             seek(cursor() + 1);
         }
     };
+
+    /**
+     * Success "type-constructor". The type that the parser returns when it
+     * succeeded.
+     */
+    template <typename T>
+    class success {
+    public:
+        using value_type = T;
+
+    private:
+        value_type  m_Value;
+        std::size_t m_Remaining;
+
+    public:
+        template <typename TFwd>
+        constexpr success(TFwd&& val, std::size_t rem)
+            noexcept(noexcept(cppcmb_fwd(val)))
+            : m_Value(cppcmb_fwd(val)), m_Remaining(rem) {
+        }
+
+        [[nodiscard]] constexpr auto value() &
+            cppcmb_return(m_Value);
+        [[nodiscard]] constexpr auto value() const&
+            cppcmb_return(m_Value);
+        [[nodiscard]] constexpr auto value() &&
+            cppcmb_return(std::move(m_Value));
+        [[nodiscard]] constexpr auto value() const&&
+            cppcmb_return(std::move(m_Value));
+
+        constexpr auto remaining() const cppcmb_return(m_Remaining);
+    };
+
+    template <typename TFwd>
+    success(TFwd&&, std::size_t) -> success<detail::remove_cvref_t<TFwd>>;
+
+    /**
+     * Failure "type-constructor". The type that the parser returns when it
+     * fails.
+     */
+    class failure {
+    private:
+        std::size_t m_Furthest;
+
+    public:
+        constexpr failure(std::size_t furth) noexcept
+            : m_Furthest(furth) {
+        }
+
+        [[nodiscard]] constexpr auto furthest() const
+            cppcmb_return(m_Furthest);
+    };
+
+    /**
+     * The result type of a parser. It's either a success or a failure type.
+     */
+    template <typename T>
+    class result {
+    public:
+        using success_type = success<T>;
+        using failure_type = failure;
+
+    private:
+        template <typename U>
+        static constexpr bool is_self_v =
+            std::is_same_v<detail::remove_cvref_t<U>, result>;
+
+        std::variant<success_type, failure_type> m_Data;
+
+    public:
+        template <typename TFwd, cppcmb_requires_t(!is_self_v<TFwd>)>
+        constexpr result(TFwd&& val) noexcept(noexcept(cppcmb_fwd(val)))
+            : m_Data(cppcmb_fwd(val)) {
+        }
+
+        [[nodiscard]] constexpr auto is_success() const
+            cppcmb_return(std::holds_alternative<success_type>(m_Data));
+
+        [[nodiscard]] constexpr auto is_failure() const
+            cppcmb_return(std::holds_alternative<failure_type>(m_Data));
+
+        [[nodiscard]] constexpr auto success() &
+            cppcmb_return(std::get<success_type>(m_Data));
+        [[nodiscard]] constexpr auto success() const&
+            cppcmb_return(std::get<success_type>(m_Data));
+        [[nodiscard]] constexpr auto success() &&
+            cppcmb_return(std::get<success_type>(std::move(m_Data)));
+        [[nodiscard]] constexpr auto success() const&&
+            cppcmb_return(std::get<success_type>(std::move(m_Data)));
+
+        [[nodiscard]] constexpr auto failure() &
+            cppcmb_return(std::get<failure_type>(m_Data));
+        [[nodiscard]] constexpr auto failure() const&
+            cppcmb_return(std::get<failure_type>(m_Data));
+        [[nodiscard]] constexpr auto failure() &&
+            cppcmb_return(std::get<failure_type>(std::move(m_Data)));
+        [[nodiscard]] constexpr auto failure() const&&
+            cppcmb_return(std::get<failure_type>(std::move(m_Data)));
+    };
+
+    namespace detail {
+
+        class combinator_base {
+        private:
+            static inline std::size_t s_ID_Counter = 0;
+
+            std::size_t m_ID = s_ID_Counter++;
+
+        public:
+            [[nodiscard]] constexpr auto parser_id() const
+                cppcmb_return(m_ID);
+        };
+
+        template <typename Self>
+        class crtp {
+        public:
+            using self_type = Self;
+
+            [[nodiscard]]
+            constexpr self_type& self() & noexcept {
+                return static_cast<self_type&>(*this);
+            }
+
+            [[nodiscard]]
+            constexpr self_type const& self() const& noexcept {
+                return static_cast<self_type const&>(*this);
+            }
+        };
+
+    } /* namespace detail */
+
+    /**
+     * Check if a type correctly defives from the combinator base.
+     */
+    template <typename T>
+    using is_combinator = std::is_base_of<detail::combinator_base, T>;
+
+    template <typename T>
+    inline constexpr bool is_combinator_v = is_combinator<T>::value;
+
+    // Forward-declare the action combinator
+    template <typename Cmb, typename Fn>
+    class action;
+
+    /**
+     * Every combinator must derive from this (so we can inject the subscript
+     * operator).
+     */
+    #define cppcmb_noexcept_subscript(...) \
+    noexcept(noexcept(action(std::declval<__VA_ARGS__>(), cppcmb_fwd(fn))))
+
+    template <typename Self>
+    class combinator : private detail::crtp<Self>,
+                       private detail::combinator_base {
+    public:
+        template <typename Fn>
+        [[nodiscard]] constexpr auto operator[](Fn&& fn) &
+        cppcmb_noexcept_subscript(Self&) {
+            return action(this->self(), cppcmb_fwd(fn));
+        }
+
+        template <typename Fn>
+        [[nodiscard]] constexpr auto operator[](Fn&& fn) const&
+        cppcmb_noexcept_subscript(Self const&) {
+            return action(this->self(), cppcmb_fwd(fn));
+        }
+
+        template <typename Fn>
+        [[nodiscard]] constexpr auto operator[](Fn&& fn) &&
+        cppcmb_noexcept_subscript(Self&&) {
+            return action(std::move(this->self()), cppcmb_fwd(fn));
+        }
+
+        template <typename Fn>
+        [[nodiscard]] constexpr auto operator[](Fn&& fn) const&&
+        cppcmb_noexcept_subscript(Self const&&) {
+            return action(std::move(this->self()), cppcmb_fwd(fn));
+        }
+    };
+
+    #undef cppcmb_noexcept_subscript
+
+    /**
+     * Action combinator, applies a function to the result when the sub-parser
+     * succeeds.
+     */
+    template <typename Cmb, typename Fn>
+    class action : public combinator<action<Cmb, Fn>> {
+    private:
+        Cmb m_Parser;
+        Fn m_Fn;
+
+    public:
+        template <typename CmbFwd, typename FnFwd>
+        constexpr action(CmbFwd&& cmb, FnFwd&& fn)
+            noexcept(noexcept(cppcmb_fwd(cmb)) && noexcept(cppcmb_fwd(fn)))
+            : m_Parser(cppcmb_fwd(cmb)), m_Fn(cppcmb_fwd(fn)) {
+        }
+        // XXX(LPeter1997): Implement
+    };
+
+    template <typename CmbFwd, typename FnFwd>
+    action(CmbFwd&&, FnFwd&&) -> action<
+        detail::remove_cvref_t<CmbFwd>,
+        std::decay_t<FnFwd>
+    >;
 
 } /* namespace cppcmb */
 
