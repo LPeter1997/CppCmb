8be0d347bf58998b6cde659900f312d0a3497850
LPeter1997 <lenkefi.peti@gmail.com>
2019-03-25 17:24:46 +0100

    Implemented the action combinator

--- cppcmb.hpp
+++ cppcmb.hpp
@@ -167,8 +167,16 @@ namespace cppcmb {
         std::size_t m_Cursor;
 
     public:
+        using value_type =
+            detail::remove_cvref_t<decltype((*m_Source)[m_Cursor])>;
+
         constexpr reader(Src const& src) noexcept
             : m_Source(std::addressof(src)), m_Cursor(0U) {
+        }
+
+        constexpr reader(Src const& src, std::size_t idx) noexcept
+            : reader(src) {
+            seek(idx);
         }
 
         reader(Src const&& src) = delete;
@@ -180,10 +188,10 @@ namespace cppcmb {
             cppcmb_return(m_Cursor);
 
         [[nodiscard]] constexpr auto is_end() const
-            cppcmb_return(cursor() == std::size(source()));
+            cppcmb_return(m_Cursor == std::size(*m_Source));
 
         [[nodiscard]] constexpr auto current() const
-            cppcmb_return(source()[cursor()]);
+            cppcmb_return((*m_Source)[m_Cursor]);
 
         constexpr void seek(std::size_t idx) noexcept {
             cppcmb_assert(
@@ -217,7 +225,7 @@ namespace cppcmb {
 
     public:
         template <typename TFwd>
-        constexpr success(TFwd&& val, std::size_t rem)
+        constexpr success(TFwd&& val, std::size_t rem = 0)
             noexcept(noexcept(cppcmb_fwd(val)))
             : m_Value(cppcmb_fwd(val)), m_Remaining(rem) {
         }
@@ -231,11 +239,15 @@ namespace cppcmb {
         [[nodiscard]] constexpr auto value() const&&
             cppcmb_return(std::move(m_Value));
 
-        constexpr auto remaining() const cppcmb_return(m_Remaining);
+        [[nodiscard]] constexpr auto remaining() const
+            cppcmb_return(m_Remaining);
     };
 
     template <typename TFwd>
     success(TFwd&&, std::size_t) -> success<detail::remove_cvref_t<TFwd>>;
+
+    template <typename TFwd>
+    success(TFwd&&) -> success<detail::remove_cvref_t<TFwd>>;
 
     /**
      * Failure "type-constructor". The type that the parser returns when it
@@ -246,7 +258,7 @@ namespace cppcmb {
         std::size_t m_Furthest;
 
     public:
-        constexpr failure(std::size_t furth) noexcept
+        constexpr failure(std::size_t furth = 0) noexcept
             : m_Furthest(furth) {
         }
 
@@ -254,11 +266,20 @@ namespace cppcmb {
             cppcmb_return(m_Furthest);
     };
 
+    namespace detail {
+
+        struct result_base {};
+
+        template <typename T>
+        inline constexpr bool is_result_v = std::is_base_of_v<result_base, T>;
+
+    } /* namespace detail */
+
     /**
      * The result type of a parser. It's either a success or a failure type.
      */
     template <typename T>
-    class result {
+    class result : private detail::result_base {
     public:
         using success_type = success<T>;
         using failure_type = failure;
@@ -303,6 +324,11 @@ namespace cppcmb {
 
     namespace detail {
 
+        /**
+         * A tag-type for every combinator.
+         * Also provides an ID that is unique for every new combinator but the
+         * same for copies. Good for cache-ing results, like the packrat parser.
+         */
         class combinator_base {
         private:
             static inline std::size_t s_ID_Counter = 0;
@@ -314,6 +340,10 @@ namespace cppcmb {
                 cppcmb_return(m_ID);
         };
 
+        /**
+         * CRTP helper.
+         * @see https://www.fluentcpp.com/2017/05/19/crtp-helper/
+         */
         template <typename Self>
         class crtp {
         public:
@@ -353,7 +383,7 @@ namespace cppcmb {
     noexcept(noexcept(action(std::declval<__VA_ARGS__>(), cppcmb_fwd(fn))))
 
     template <typename Self>
-    class combinator : private detail::crtp<Self>,
+    class combinator : public detail::crtp<Self>,
                        private detail::combinator_base {
     public:
         template <typename Fn>
@@ -383,28 +413,132 @@ namespace cppcmb {
 
     #undef cppcmb_noexcept_subscript
 
+    namespace detail {
+
+        /**
+         * Concept check for parser interface.
+         */
+        template <typename T, typename Src>
+        using apply_t = decltype(
+            std::declval<T>().apply(std::declval<reader<Src> const&>())
+        );
+
+        template <typename T, typename Src>
+        inline constexpr bool has_parser_interface_v = std::conjunction_v<
+            is_detected<apply_t, T, Src>,
+            is_combinator<T>
+        >;
+
+    } /* namespace detail */
+
+// We will not undef this as users could use it
+#define cppcmb_assert_parser(p, src)                  \
+static_assert(                                        \
+    ::cppcmb::detail::has_parser_interface_v<p, src>, \
+    "A parser must be derived from combinator<Self> " \
+    " and have a member function apply(reader<Src>)!" \
+)
+
+    /**
+     * Helper to get the result value of a parse success.
+     */
+    template <typename P, typename Src>
+    using parser_value_t = detail::remove_cvref_t<decltype(
+        std::declval<P>().apply(std::declval<reader<Src>&>())
+            .success().value()
+    )>;
+
     /**
      * Action combinator, applies a function to the result when the sub-parser
      * succeeds.
      */
-    template <typename Cmb, typename Fn>
-    class action : public combinator<action<Cmb, Fn>> {
+    template <typename P, typename Fn>
+    class action : public combinator<action<P, Fn>> {
     private:
-        Cmb m_Parser;
+        P  m_Parser;
         Fn m_Fn;
 
     public:
-        template <typename CmbFwd, typename FnFwd>
-        constexpr action(CmbFwd&& cmb, FnFwd&& fn)
+        template <typename PFwd, typename FnFwd>
+        constexpr action(PFwd&& cmb, FnFwd&& fn)
             noexcept(noexcept(cppcmb_fwd(cmb)) && noexcept(cppcmb_fwd(fn)))
             : m_Parser(cppcmb_fwd(cmb)), m_Fn(cppcmb_fwd(fn)) {
         }
-        // XXX(LPeter1997): Implement
+
+        // XXX(LPeter1997): Noexcept specifier?
+        template <typename Src>
+        [[nodiscard]]
+        constexpr decltype(auto) apply(reader<Src> const& src) const {
+            cppcmb_assert_parser(P, Src);
+
+            using value_t = parser_value_t<P, Src>;
+
+            static_assert(
+                std::is_invocable_v<Fn, value_t>,
+                "The given action function must be invocable with the parser's "
+                "successful value type!"
+            );
+
+            using fn_result_t = std::invoke_result_t<Fn, value_t>;
+            using dispatch_tag =
+                std::bool_constant<detail::is_result_v<fn_result_t>>;
+
+            return apply_impl<fn_result_t>(src, dispatch_tag());
+        }
+
+    private:
+        // XXX(LPeter1997): Noexcept specifier?
+        // Action can fail
+        template <typename FRes, typename Src>
+        [[nodiscard]] constexpr auto apply_impl(
+            reader<Src> const& src,
+            std::true_type) const -> FRes {
+
+            auto inv = m_Parser.apply(src);
+            if (inv.is_failure()) {
+                // Early failure
+                return std::move(inv).failure();
+            }
+
+            auto succ = std::move(inv).success();
+            // Try to apply the action
+            auto act_inv = m_Fn(std::move(succ).value());
+            // In any case we will have to decorate the result with the position
+            if (act_inv.is_failure()) {
+                return failure(src.cursor());
+            }
+            else {
+                return success(
+                    std::move(act_inv).success().value(),
+                    succ.remaining()
+                );
+            }
+        }
+
+        // XXX(LPeter1997): Noexcept specifier?
+        // Action can't fail
+        template <typename FRes, typename Src>
+        [[nodiscard]] constexpr auto apply_impl(
+            reader<Src> const& src,
+            std::false_type) const -> result<FRes> {
+
+            auto inv = m_Parser.apply(src);
+            if (inv.is_failure()) {
+                // Early failure
+                return std::move(inv).failure();
+            }
+
+            auto succ = std::move(inv).success();
+            // Apply the action
+            auto act_val = m_Fn(std::move(succ).value());
+            // Wrap it in a success
+            return success(std::move(act_val), succ.remaining());
+        }
     };
 
-    template <typename CmbFwd, typename FnFwd>
-    action(CmbFwd&&, FnFwd&&) -> action<
-        detail::remove_cvref_t<CmbFwd>,
+    template <typename PFwd, typename FnFwd>
+    action(PFwd&&, FnFwd&&) -> action<
+        detail::remove_cvref_t<PFwd>,
         std::decay_t<FnFwd>
     >;
 
