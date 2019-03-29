267f58cf9fafc5a12399a11fa894a18a9e050752
LPeter1997 <lenkefi.peti@gmail.com>
2019-03-29 08:20:06 +0100

    Added sum utility to coerce to sum

--- cppcmb.hpp
+++ cppcmb.hpp
@@ -537,6 +537,70 @@ namespace cppcmb {
 
     namespace detail {
 
+        template <typename T, typename... Ts>
+        inline constexpr bool contains_type_v = (... || std::is_same_v<T, Ts>);
+
+        ////////////////////////////////////////////////////
+
+        template <typename...>
+        struct sum_values_t_impl;
+
+        template <typename T>
+        struct sum_values_t_impl<sum<T>> {
+            using type = T;
+        };
+
+        template <typename... Ts>
+        struct sum_values_t_impl<sum<Ts...>> {
+            using type = sum<Ts...>;
+        };
+
+        template <typename... Ts, typename... Us, typename... Vs>
+        struct sum_values_t_impl<sum<Ts...>, sum<Us...>, Vs...> {
+            using type =
+                typename sum_values_t_impl<sum<Ts...>, Us..., Vs...>::type;
+        };
+
+        template <typename... Ts, typename Head, typename... Tail>
+        struct sum_values_t_impl<sum<Ts...>, Head, Tail...> {
+            using type = std::conditional_t<
+                contains_type_v<Head, Ts...>,
+                typename sum_values_t_impl<sum<Ts...>, Tail...>::type,
+                typename sum_values_t_impl<sum<Ts..., Head>, Tail...>::type
+            >;
+        };
+
+    } /* namespace detail */
+
+    template <typename... Ts>
+    using sum_values_t = typename detail::sum_values_t_impl<sum<>, Ts...>::type;
+
+    namespace detail {
+
+        template <typename RetT, typename T>
+        constexpr auto sum_values_impl(std::false_type, T&& val)
+            cppcmb_return(RetT(cppcmb_fwd(val)));
+
+        // XXX(LPeter1997): Noexcept specifier
+        template <typename RetT, typename T>
+        constexpr decltype(auto) sum_values_impl(std::true_type, T&& val) {
+            return std::visit(
+                [](auto&& v) -> RetT { return RetT(cppcmb_fwd(v)); },
+                cppcmb_fwd(val).as_variant()
+            );
+        }
+
+    } /* namespace detail */
+
+    template <typename... Ts, typename T>
+    constexpr auto sum_values(T&& val)
+        cppcmb_return(detail::sum_values_impl<sum_values_t<Ts...>>(
+            detail::is_sum<detail::remove_cvref_t<T>>(),
+            cppcmb_fwd(val)
+        ));
+
+    namespace detail {
+
         // Arg is a product
         template <typename Fn, typename T>
         [[nodiscard]] constexpr auto apply_value_impl(
