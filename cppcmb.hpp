8cdb8940e46f15f5d0f4209d69f6d24bd74025ab
LPeter1997 <lenkefi.peti@gmail.com>
2019-04-01 20:01:47 +0200

    Added the 'many' combinator

--- cppcmb.hpp
+++ cppcmb.hpp
@@ -16,6 +16,7 @@
 #include <type_traits>
 #include <utility>
 #include <variant>
+#include <vector>
 
 /**
  * Macros used by the library.
@@ -1210,6 +1211,116 @@ static_assert(                                        \
         cppcmb_requires_t(detail::is_combinator_cvref_v<P2>)>
     [[nodiscard]] constexpr auto operator||(pass_t, P2&& p2)
         cppcmb_return(cppcmb_fwd(p2));
+
+    /**
+     * A type-pack that describes a collection except it's type.
+     * Used for the many and many1 combinators.
+     */
+    template <
+        template <typename...> typename Coll,
+        template <typename> typename... Ts
+    >
+    struct collect_to_t {
+        template <typename T>
+        using type = Coll<T, Ts<T>...>;
+    };
+
+    template <
+        template <typename...> typename Coll,
+        template <typename> typename... Ts
+    >
+    inline constexpr auto collect_to = collect_to_t<Coll, Ts...>();
+
+    namespace detail {
+
+        /**
+         * Tag-type for many and many1.
+         */
+        struct many_tag {};
+
+        /**
+         * SFINAE for many types.
+         */
+        template <typename T>
+        inline constexpr bool is_many_v = std::is_base_of_v<many_tag, T>;
+
+    } /* namespace detail */
+
+    /**
+     * A parser that applies a parser as many times as it can, before it fails.
+     * Collects the results into some container. Always succeeds.
+     */
+    template <typename P, typename To = collect_to_t<std::vector>>
+    class many_t : public combinator<many_t<P>>,
+                   private detail::many_tag {
+    private:
+        template <typename Src>
+        using value_t = typename To::template type<parser_value_t<P, Src>>;
+
+        P m_Parser;
+
+    public:
+        // XXX(LPeter1997): Noexcept specifier
+        template <typename PFwd>
+        constexpr many_t(PFwd&& p)
+            : m_Parser(cppcmb_fwd(p)) {
+        }
+
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) &
+            cppcmb_return(many_t<P, To2>(m_Parser));
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) const&
+            cppcmb_return(many_t<P, To2>(m_Parser));
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) &&
+            cppcmb_return(many_t<P, To2>(std::move(m_Parser)));
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) const&&
+            cppcmb_return(many_t<P, To2>(std::move(m_Parser)));
+
+        // XXX(LPeter1997): Noexcept specifier
+        template <typename Src>
+        [[nodiscard]] constexpr auto apply(reader<Src> const& r) const
+            -> result<value_t<Src>> {
+            cppcmb_assert_parser(P, Src);
+
+            auto coll = value_t<Src>();
+            auto rr = r;
+            while (true) {
+                auto p_inv = m_Parser.apply(rr);
+                if (p_inv.is_failure()) {
+                    // Stop applying
+                    break;
+                }
+                auto p_succ = std::move(p_inv).success();
+                // Add to collection
+                coll.push_back(std::move(p_succ).value());
+                // Move reader
+                rr.seek(p_succ.remaining());
+            }
+            return success(std::move(coll), rr.cursor());
+        }
+    };
+
+    template <typename PFwd>
+    many_t(PFwd&&) -> many_t<detail::remove_cvref_t<PFwd>>;
+
+    /**
+     * Operator for making many parser.
+     */
+    template <typename P,
+        cppcmb_requires_t(detail::is_combinator_cvref_v<P>)>
+    [[nodiscard]] constexpr auto operator*(P&& p)
+        cppcmb_return(many_t(cppcmb_fwd(p)));
+
+    /**
+     * Operator to collect 'many' and 'many1' to a different container.
+     */
+    template <typename P, typename To,
+        cppcmb_requires_t(detail::is_many_v<detail::remove_cvref_t<P>>)>
+    [[nodiscard]] constexpr auto operator>>(P&& p, To to)
+        cppcmb_return(cppcmb_fwd(p).collect_to(to));
 
 } /* namespace cppcmb */
 
