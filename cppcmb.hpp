4573fb4b8e21a264a303aff2fc326b91119c000f
LPeter1997 <lenkefi.peti@gmail.com>
2019-04-01 21:00:53 +0200

    Added many1 combinator

--- cppcmb.hpp
+++ cppcmb.hpp
@@ -1279,6 +1279,15 @@ static_assert(                                        \
         [[nodiscard]] constexpr auto collect_to(To2) const&&
             cppcmb_return(many_t<P, To2>(std::move(m_Parser)));
 
+        [[nodiscard]] constexpr auto underlying() &
+            cppcmb_return(m_Parser);
+        [[nodiscard]] constexpr auto underlying() const&
+            cppcmb_return(m_Parser);
+        [[nodiscard]] constexpr auto underlying() &&
+            cppcmb_return(std::move(m_Parser));
+        [[nodiscard]] constexpr auto underlying() const&&
+            cppcmb_return(std::move(m_Parser));
+
         // XXX(LPeter1997): Noexcept specifier
         template <typename Src>
         [[nodiscard]] constexpr auto apply(reader<Src> const& r) const
@@ -1313,6 +1322,74 @@ static_assert(                                        \
         cppcmb_requires_t(detail::is_combinator_cvref_v<P>)>
     [[nodiscard]] constexpr auto operator*(P&& p)
         cppcmb_return(many_t(cppcmb_fwd(p)));
+    /**
+     * A parser that applies a parser as many times as it can, before it fails.
+     * Only succeeds if the parser could be applied at least once.
+     */
+    template <typename P, typename To = collect_to_t<std::vector>>
+    class many1_t : public combinator<many1_t<P>>,
+                    private detail::many_tag {
+    private:
+        template <typename Src>
+        using value_t = parser_value_t<many_t<P, To>, Src>;
+
+        many_t<P, To> m_Parser;
+
+    public:
+        // XXX(LPeter1997): Noexcept specifier
+        template <typename PFwd>
+        constexpr many1_t(PFwd&& p)
+            : m_Parser(many_t<P, To>(cppcmb_fwd(p))) {
+        }
+
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) &
+            cppcmb_return(many1_t<P, To2>(m_Parser.underlying()));
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) const&
+            cppcmb_return(many1_t<P, To2>(m_Parser.underlying()));
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) &&
+            cppcmb_return(many1_t<P, To2>(std::move(m_Parser).underlying()));
+        template <typename To2>
+        [[nodiscard]] constexpr auto collect_to(To2) const&&
+            cppcmb_return(many1_t<P, To2>(std::move(m_Parser).underlying()));
+
+        // XXX(LPeter1997): Noexcept specifier
+        template <typename Src>
+        [[nodiscard]] constexpr auto apply(reader<Src> const& r) const
+            -> result<value_t<Src>> {
+            cppcmb_assert_parser(P, Src);
+
+            auto p_inv = m_Parser.apply(r);
+
+            cppcmb_assert(
+                "The underlying 'many' parser must always succeed!",
+                p_inv.is_success()
+            );
+
+            auto p_succ = std::move(p_inv).success();
+            if (p_succ.value().size() > 0) {
+                // Succeed
+                return p_succ;
+            }
+            else {
+                // Fail
+                return failure(r.cursor());
+            }
+        }
+    };
+
+    template <typename PFwd>
+    many1_t(PFwd&&) -> many1_t<detail::remove_cvref_t<PFwd>>;
+
+    /**
+     * Operator for making many parser.
+     */
+    template <typename P,
+        cppcmb_requires_t(detail::is_combinator_cvref_v<P>)>
+    [[nodiscard]] constexpr auto operator+(P&& p)
+        cppcmb_return(many1_t(cppcmb_fwd(p)));
 
     /**
      * Operator to collect 'many' and 'many1' to a different container.
