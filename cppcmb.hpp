e3872a31576e423d9d24a8a939f863a5c099d82f
LPeter1997 <lenkefi.peti@gmail.com>
2019-04-22 10:06:07 +0200

    Implemented grouping

--- cppcmb.hpp
+++ cppcmb.hpp
@@ -2,7 +2,7 @@
  * cppcmb.hpp
  *
  * This file has been merged from multiple source files.
- * Generation date: 2019-04-22 09:09:40.677458
+ * Generation date: 2019-04-22 10:04:53.793698
  *
  * Copyright (c) 2018-2019 Peter Lenkefi
  * Distributed under the MIT License.
@@ -2766,21 +2766,37 @@ namespace detail {
 namespace regex {
 
 /**
- * <top>    ::= <term> '|' <top>
- *            | <term>
- *            ;
+ * <top>           ::= <term> '|' <top>
+ *                   | <term>
+ *                   ;
  *
- * <term>   ::= <factor> <term>
- *            ;
+ * <term>          ::= <factor> <term>
+ *                   | <factor>
+ *                   ;
  *
- * <factor> ::= <atom> '*'?
- *            ;
+ * <factor>        ::= <atom> '*'
+ *                   | <atom> '+'
+ *                   | <atom> '?'
+ *                   | <atom>
+ *                   ;
  *
- * <atom>   ::= CHAR
- *            | '\' CHAR
- *            | <char_grouping>
- *            | '(' <top> ')'
- *            ;
+ * <atom>          ::= '(' <top> ')'
+ *                   | '[' <char_grouping> ']'
+ *                   | <literal>
+ *                   ;
+ *
+ * <char_grouping> ::= <group_element> <char_grouping>
+ *                   | <group_element>
+ *                   ;
+ *
+ * <group_element> ::= '\' '-'
+ *                   | <literal> '-' <literal>
+ *                   | <literal>
+ *                   ;
+ *
+ * <literal>       ::= CHAR
+ *                   | '\' SPECIAL_CHAR
+ *                   ;
  */
 
 template <char Ch>
@@ -2788,6 +2804,15 @@ constexpr bool is_char(char c) { return c == Ch; }
 
 template <char Ch>
 inline constexpr auto ch = one[filter(is_char<Ch>)][select<>];
+
+template <char Ch1, char Ch2>
+constexpr bool is_range(char c) {
+    static_assert(Ch1 <= Ch2);
+    return c >= Ch1 && c <= Ch2;
+}
+
+template <char Ch1, char Ch2>
+inline constexpr auto range = one[filter(is_range<Ch1, Ch2>)][select<>];
 
 // XXX(LPeter1997): We publish something like this in the API
 /**
@@ -2819,6 +2844,11 @@ struct parser {
     }
 
     template <typename T>
+    [[nodiscard]] static constexpr auto qmark(T p) noexcept {
+        return action_t(-p, select<>);
+    }
+
+    template <typename T>
     static constexpr bool is_failure(T) {
         return std::is_same_v<remove_cvref_t<T>, failure>;
     }
@@ -2829,11 +2859,11 @@ struct parser {
     }
 
     [[nodiscard]] static constexpr bool is_special(char ch) noexcept {
-        return ch == '('
-            || ch == ')'
-            || ch == '|'
-            || ch == '*'
-            || ch == '+'
+        return ch == '(' || ch == ')'
+            || ch == '[' || ch == ']'
+            || ch == '*' || ch == '+'
+            || ch == '|' || ch == '?'
+            || ch == '\\'
             ;
     }
 
@@ -2901,6 +2931,9 @@ struct parser {
             else if constexpr (curr == '+') {
                 return success(plus(lhs.value()), lhs.matched() + 1);
             }
+            else if constexpr (curr == '?') {
+                return success(qmark(lhs.value()), lhs.matched() + 1);
+            }
             else {
                 return lhs;
             }
@@ -2919,21 +2952,109 @@ struct parser {
                 static_assert(char_at<NextIdx>(src) == ')');
                 return success(sub.value(), sub.matched() + 2);
             }
-            else if constexpr (curr == '\\') {
-                constexpr char nxt = char_at<Idx + 1>(src);
-                static_assert(is_special(nxt));
-                return success(ch<nxt>, 2);
-            }
-            else if constexpr (is_special(curr)) {
-                // Special characters
-                return failure();
+            else if constexpr (curr == '[') {
+                // Character classes
+                constexpr auto sub = char_grouping<Idx + 1>(src);
+                static_assert(!is_failure(sub));
+                constexpr std::size_t NextIdx = Idx + 1 + sub.matched();
+                static_assert(char_at<NextIdx>(src) == ']');
+                return success(sub.value(), sub.matched() + 2);
             }
             else {
-                return success(ch<curr>, 1);
+                return literal<Idx>(src);
             }
         }
         else {
             failure();
+        }
+    }
+
+    template <std::size_t Idx, typename Src>
+    [[nodiscard]] static constexpr auto char_grouping(Src src) noexcept {
+        constexpr auto lhs = group_element<Idx>(src);
+        static_assert(!is_failure(lhs));
+        return char_grouping_impl<Idx + lhs.matched()>(lhs, src);
+    }
+
+    template <std::size_t Idx, typename Res, typename Src>
+    [[nodiscard]]
+    static constexpr auto char_grouping_impl(Res res, Src src) noexcept {
+        constexpr auto lhs = group_element<Idx>(src);
+        if constexpr (is_failure(lhs)) {
+            return res;
+        }
+        else {
+            return char_grouping_impl<Idx + lhs.matched()>(
+                success(
+                    res.value() | lhs.value(),
+                    res.matched() + lhs.matched()
+                ),
+                src
+            );
+        }
+    }
+
+    template <std::size_t Idx, typename Src>
+    [[nodiscard]] static constexpr auto group_element(Src src) noexcept {
+        if constexpr (char_at<Idx>(src) == '\\'
+                   && char_at<Idx + 1>(src) == '-') {
+            return success(ch<'-'>, 2);
+        }
+        else {
+            constexpr auto lit = literal_ch<Idx>(src);
+            if constexpr (is_failure(lit)) {
+                return failure();
+            }
+            else {
+                constexpr std::size_t NextIdx = Idx + lit.matched();
+                if constexpr (char_at<NextIdx>(src) == '-') {
+                    constexpr auto lit2 = literal_ch<NextIdx + 1>(src);
+                    if constexpr (is_failure(lit2)) {
+                        // No right-hand-side, only consumed lit
+                        return success(ch<lit.value()>, lit.matched());
+                    }
+                    else {
+                        // Char range
+                        return success(
+                            range<lit.value(), lit2.value()>,
+                            lit.matched() + 1 + lit2.matched()
+                        );
+                    }
+                }
+                else {
+                    return success(ch<lit.value()>, lit.matched());
+                }
+            }
+        }
+    }
+
+    template <std::size_t Idx, typename Src>
+    [[nodiscard]] static constexpr auto literal(Src src) noexcept {
+        constexpr auto lc = literal_ch<Idx>(src);
+        if constexpr (is_failure(lc)) {
+            return failure();
+        }
+        else {
+            return success(ch<lc.value()>, lc.matched());
+        }
+    }
+
+    template <std::size_t Idx, typename Src>
+    [[nodiscard]] static constexpr auto literal_ch(Src src) noexcept {
+        constexpr char curr = char_at<Idx>(src);
+        if constexpr (curr == '\\') {
+            // Escaped
+            constexpr char nxt = char_at<Idx + 1>(src);
+            static_assert(is_special(nxt));
+            return success(nxt, 2);
+        }
+        else if constexpr (is_special(curr)) {
+            // Special characters
+            return failure();
+        }
+        else {
+            // Literal match
+            return success(curr, 1);
         }
     }
 };
